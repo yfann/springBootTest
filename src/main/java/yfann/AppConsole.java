dccc1721ab14fbd09c455dba3a4ff371e3dabb0b
yfann <yfann2012@163.com>
2019-07-26 14:03:55 +0800

    update

---
+++ src/main/java/yfann/AppConsole.java
@@ -0,0 +1,14 @@
+package yfann;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class AppConsole
+{
+    public static void main( String[] args )
+    {
+        LogTest log=new LogTest();
+    }
+}
---
+++ src/main/java/yfann/LogTest.java
@@ -0,0 +1,10 @@
+package yfann;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogTest {
+    public LogTest(){
+        log.error("error test");
+    }
+}
